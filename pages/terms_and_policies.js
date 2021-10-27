430a092176949bdbeb91fd5b34ab7c13011fb12b
Zeha Irawan <zehairawan@gmail.com>
2021-10-27 20:09:52 +0800

    format privacy policy and terms

--- pages/terms_and_policies.js
+++ pages/terms_and_policies.js
@@ -1,40 +1,45 @@
-import React from 'react'
-import Head from 'next/head'
+import Head from 'next/head';
+import React from 'react';
 
 const terms = () => {
   return (
-    <div>
+    <div className="container">
       <Head>
         <title>Term of Services | Sotomate</title>
         <meta property="og:title" content="Term of Services" />
       </Head>
-      Terms of Service End-User License Agreement You are welcome to use the
-      TubeTotal Community Management Service Platform (hereinafter referred to
-      as Party A) and the members of the Linghui Digital Co., Ltd. (hereinafter
-      referred to as Party B). Please read this Terms of Service carefully
-      before you register as a TubeTotal Community Business Service Platform or
-      join a free or paid service (hereafter referred to as the Service). By
-      completing the registration of individual members or using the services on
-      this site, you have read and agreed to all the contents of these Terms of
-      Service and related activities, agree to abide by the relevant
-      specifications, and have accepted Party A's platform may be due to third
-      parties. The social media platform is constantly updated or guarded
-      against force majeure factors, resulting in some services not working.
-      Party B is aware that it will not be able to request a refund. Please read
-      this agreement carefully before using this product. If you agree to all of
-      the terms of this End-User License Agreement, by checking the box or
-      clicking the button to confirm your acceptance when you first use the web
-      application, you are agreeing to all the terms of this agreement. Also, By
-      Registering as TubeTotal's member, you accept and agree to be bound by the
-      terms of this End-User License Agreement, you are agreeing to all the
-      terms of this agreement. Note: We are using the official Social Media API
-      (Facebook, Twitter etc, except Instagram) which is available on Developer
-      Center. That is a reason why this app depends on Social Media API
-      (Facebook, Instagram, Twitter etc). Are not responsible if they made too
-      many critical changes in their side. We also don't guarantee that the
-      compatibility of the app with Social Media API will be forever. We don't
-      provide any refund for all problems which are originated from Social Media
-      API (Facebook, Instagram, Twitter, Youtube, Pinterest etc).
+
+      <h2>Terms of Service End-User License Agreement</h2>
+      <p>
+        You are welcome to use the TubeTotal Community Management Service
+        Platform (hereinafter referred to as Party A) and the members of the
+        Linghui Digital Co., Ltd. (hereinafter referred to as Party B). </p> <p>Please
+        read this Terms of Service carefully before you register as a TubeTotal
+        Community Business Service Platform or join a free or paid service
+        (hereafter referred to as the Service). </p> <p>By completing the registration
+        of individual members or using the services on this site, you have read
+        and agreed to all the contents of these Terms of Service and related
+        activities, agree to abide by the relevant specifications, and have
+        accepted Party A's platform may be due to third parties. The social
+        media platform is constantly updated or guarded against force majeure
+        factors, resulting in some services not working. Party B is aware that
+        it will not be able to request a refund. </p><p>Please read this agreement
+        carefully before using this product.</p> <p> If you agree to all of the terms of
+        this End-User License Agreement, by checking the box or clicking the
+        button to confirm your acceptance when you first use the web
+        application, you are agreeing to all the terms of this agreement. Also,
+        By Registering as TubeTotal's member, you accept and agree to be bound
+        by the terms of this End-User License Agreement, you are agreeing to all
+        the terms of this agreement. </p> <p>Note: We are using the official Social
+        Media API (Facebook, Twitter etc, except Instagram) which is available
+        on Developer Center. That is a reason why this app depends on Social
+        Media API (Facebook, Instagram, Twitter etc). Are not responsible if
+        they made too many critical changes in their side. We also don't
+        guarantee that the compatibility of the app with Social Media API will
+        be forever. We don't provide any refund for all problems which are
+        originated from Social Media API (Facebook, Instagram, Twitter, Youtube,
+        Pinterest etc).
+      </p>
     </div>
   );
 }
