cee1b3510df2bacc2fa81a3aa96958a2bfa363c3
Zeha Irawan <zehairawan@gmail.com>
2021-11-04 06:39:05 +0800

    add hamburger

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import { UserContext } from '@lib/context';
 import { auth } from '@lib/firebase';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
@@ -15,23 +15,32 @@ export default function Navbar() {
     router.push('sign-in');
   };
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className={[styles.navbar]}>
-      <ul>
-          <Link href="/">
-            <h1>Sotomate</h1>
-          </Link>
+
+    <ul className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+
+      <ul className={isOpen ? styles.show : styles.hide  }>
+        <Link href="/">
+          <h1>Sotomate</h1>
+        </Link>
 
         {/* <ul> */}
-          <li>
-            <span>Pricing</span>
-          </li>
-          <li>
-            <span>Features</span>
-          </li>
-          <li>
-            <span>About</span>
-          </li>
+        <li>
+          <span>Pricing</span>
+        </li>
+        <li>
+          <span>Features</span>
+        </li>
+        <li>
+          <span>About</span>
+        </li>
         {/* </ul> */}
 
         {/* Authenticated User */}
@@ -52,14 +61,14 @@ export default function Navbar() {
           <ul>
             <li>
               <Link href="/sign-in">
-                <button id="login">
-                  Login
-                </button>
+                <button className={styles.signin_button}>Login</button>
               </Link>
             </li>
             <li>
               <Link href="/sign-up">
-                <button id="signup">Signup</button>
+                <button className={styles.signup_button}>
+                  Signup
+                </button>
               </Link>
             </li>
           </ul>
