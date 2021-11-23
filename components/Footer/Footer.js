cd4374fed8af04e42e624c294ff4ee76718b8080
Zeha Irawan <zehairawan@gmail.com>
2021-11-24 07:38:46 +0800

    init footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import Link from 'next/link';
+import styles from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <ul>
-      <li>
+    <footer className={styles.footer}>
+      <h1>Why Sotomotate?</h1>
+      <ul className={styles.list}>
+        <li>
+          <Link href="/">
+            <a>Features</a>
+          </Link>
+        </li>
+
+        <li>
+
         <Link href="/">
-          <a>Home</a>
+          <a>Pricing</a>
         </Link>
-      </li>
-      <li>
-        <Link href="/privacy_policy">
-          <a>Privacy Policy</a>
+        </li>
+
+        <li>
+        <Link href="/">
+          <a>Contact Us</a>
         </Link>
-      </li>
-      <li>
-        <Link href="/terms_and_policies">
-          <a>Terms of Services</a>
-        </Link>
-      </li>
-    </ul>
+        </li>
+      </ul>
+
+
+
+          <Link href="/privacy_policy">
+            <a>Privacy Policy</a>
+          </Link>
+
+          <Link href="/terms_and_policies">
+            <a>Terms of Services</a>
+          </Link>
+
+    </footer>
   );
 }
 
