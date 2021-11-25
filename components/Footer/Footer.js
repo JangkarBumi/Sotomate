6a7754b55a15afb664724c989bf3b064c97c7768
Zeha Irawan <zehairawan@gmail.com>
2021-11-26 06:49:38 +0800

    add more links

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -5,38 +5,68 @@ import styles from './Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h1>Why Sotomotate?</h1>
-      <ul className={styles.list}>
+      <h1>About</h1>
+
+      <ul>
         <li>
+          
           <Link href="/">
-            <a>Features</a>
+            <a>Pricing</a>
+          </Link>
+        </li>
+        <li>
+          
+          <Link href="/">
+            <a>Contact Us</a>
           </Link>
         </li>
 
         <li>
-
-        <Link href="/">
-          <a>Pricing</a>
-        </Link>
-        </li>
-
-        <li>
-        <Link href="/">
-          <a>Contact Us</a>
-        </Link>
-        </li>
-      </ul>
-
-
-
           <Link href="/privacy_policy">
             <a>Privacy Policy</a>
           </Link>
-
+        </li>
+        <li>
           <Link href="/terms_and_policies">
             <a>Terms of Services</a>
           </Link>
+        </li>
+      </ul>
 
+      <h1>Products</h1>
+
+      <ul>
+        <li>
+          <Link href="/privacy_policy">
+            <a>Facebook Post Scheduler</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/terms_and_policies">
+            <a>Tweet Scheduler</a>
+          </Link>
+        </li>
+      </ul>
+
+      <h1>Use Cases</h1>
+
+      <ul>
+        <li>
+          <Link href="/privacy_policy">
+            <a>Social Media Manager</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/terms_and_policies">
+            <a>Creator</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/terms_and_policies">
+            <a>Small Business owner</a>
+          </Link>
+        </li>
+      </ul>
     </footer>
   );
 }
